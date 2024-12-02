a470fac4cb50b135cda260d23d4e68b78868ccc1
AyushiDhamane <ayushidhamane03@gmail.com>
2024-12-02 05:30:21 +0530

    code

---
+++ Downloads/last/main.py
@@ -0,0 +1,15 @@
+# Import the Streamlit library
+import streamlit as st
+
+# Set the title of the app
+st.title("Welcome to My Simple Streamlit!")
+
+# Add a text input for the user's name
+name = st.text_input("Enter your name:")
+
+# Add a button to greet the user
+if st.button("Submit"):
+    if name:
+        st.write(f"Hello, {name}! 👋 Welcome to Streamlit!")
+    else:
+        st.write("Please enter your name: ")
